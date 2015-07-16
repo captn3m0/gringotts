670db028e25c076244309a507348598fba20dd61
Abhay Rana <capt.n3m0@gmail.com>
2015-07-17 00:04:41 +0530

    splitwise time refactor

--- parser.rb
+++ parser.rb
@@ -4,12 +4,9 @@ require 'pp'
 
 class Parser
     def splitwise(body, user_id)
+      @splitwise_user_id = user_id
       expenses = JSON.parse(body)['expenses']
-      expenses.map! do |expense|
-        # convert timestamp to local timezone
-        expense['time'] = DateTime.iso8601(expense['date']).to_time.to_datetime.to_time
-        clean_splitwise_expense(expense, user_id)
-      end
+      expenses.map! {|e| clean_splitwise_expense(e)}
       summarize expenses.reject {|e| e==false}
     end
 
@@ -31,20 +28,33 @@ class Parser
       if data.is_a? Hash and data.has_key? 'orders'
         data = data.orders
       end
-      #data.eac
     end
 
-    def clean_splitwise_expense(expense, user_id)
+
+    def clean_paytm_expense(expense)
+      # We do not count wallet or failed expenses
+      return false if expense['status'].downcase === 'failed'
+      return false if expense['items'][0]['product']['vertical_label'].downcase === 'wallet'
+      return {
+        'id'                => expense['order_id'],
+        'description'       => expense['order_name'],
+        'amount'            => expense['amount'],
+        'category'          => expense['items'][0]['product']['vertical_label'],
+        'time'              => DateTime.parse(expense['dateString']).to_time
+      }
+    end
+
+    def clean_splitwise_expense(expense)
       users = expense['users']
       amount = 0
       users.each do |user|
-        if user['user_id'] === user_id
+        if user['user_id'] === @splitwise_user_id
           return false if user['owed_share'].to_f.floor.zero?
           return {
             "id"             => expense['id'],
             "description"    => expense['description'],
             "amount"         => user['owed_share'].to_f,
-            "time"           => expense['time'],
+            "time"           => DateTime.iso8601(expense['date']).to_time.to_datetime.to_time,
             "category"       => expense['category']['name']
           }
         end
